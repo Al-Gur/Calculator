cd86055c9a1ea07d92436a41c912965a7fffb913
Alexander <a@a>
2024-12-28 04:51:49 +0300

    Without reflections

---
+++ src/calculator/Calc.java
@@ -0,0 +1,20 @@
+package calculator;
+
+class Calc {
+    static double Add(double x, double y) {
+        return x + y;
+    }
+
+    static double Subtract(double x, double y) {
+        return x - y;
+    }
+
+    // Additional operations can be added here!
+    // The syntax of an operation must be as:
+    //
+    //  static double operationName(double x, double y) {
+    //    ... the body of the operation with the arguments x, y
+    //    return result;
+    //  }
+
+}
---
+++ src/calculator/CalculatorMain.java
@@ -0,0 +1,42 @@
+package calculator;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Error: 3 arguments must be entered in command line!");
+            return;
+        }
+        double x = Double.NaN;
+        double y = Double.NaN;
+        double res;
+        String operation = "";
+        try {
+            x = Double.parseDouble(args[0]);
+            y = Double.parseDouble(args[1]);
+            operation = args[2];
+        } catch (Exception e) {
+            System.out.println("Error: type of first 2 arguments must be double!");
+        }
+
+        try {
+            switch (operation) {
+                case "Add":
+                    res = Calc.Add(x, y);
+                    break;
+                case "Subtract":
+                    res = Calc.Subtract(x, y);
+                    break;
+                default:
+                    System.out.println("Error: 3th argument denotes unknown operation!");
+                    return;
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return;
+        }
+
+        System.out.println("Arguments: " + x + ", " + y);
+        System.out.println("Operation: " + operation);
+        System.out.println("Result = " + res);
+    }
+}
